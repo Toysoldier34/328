a6ce79998b5d84cf0b60e0c6352f3585af42c57c
Tony <tony.g.thompson@gmail.com>
2018-01-12 11:49:41 -0800

    htaccess

--- inclass/routing-demo/index.php
+++ inclass/routing-demo/index.php
@@ -7,6 +7,10 @@ require_once ('vendor/autoload.php');
 //create an instance of the Base class
 $f3 = Base::instance();
 
+//set debug level
+$f3->set('DEBUG', 3);
+
+
 //define a default route
 $f3->route('GET /', function() {
    /*
@@ -16,6 +20,24 @@ $f3->route('GET /', function() {
     echo '<h1>Hello, $f3 route!<h1>';
    */
    echo '<h1>Routing Demo</h1>';
+}
+);
+
+//define a page1 route
+$f3->route('GET /page1', function() {
+    echo '<h1>This is page 1</h1>';
+}
+);
+
+//define a page1 route
+$f3->route('GET /page1/subpage-a', function() {
+    echo '<h1>This is page 1, subpage a</h1>';
+}
+);
+
+//define a page2 route
+$f3->route('GET /page2', function() {
+    echo '<h1>This is page 2</h1>';
 }
 );
 
