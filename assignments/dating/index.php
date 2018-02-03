1313410c6c84816711d3a3b274730fb6b99ce3c9
Tony <tony.g.thompson@gmail.com>
2018-02-02 16:24:00 -0800

    adding interests page

--- assignments/dating/index.php
+++ assignments/dating/index.php
@@ -25,22 +25,28 @@ $f3->set('DEBUG', 3);
 
 //define a default route
 $f3->route('GET|POST /', function() {
-    echo "TestHome4";
     $template = new Template();
     echo $template->render('views/home.html');
 }
 );
 
 //define a default route
-$f3->route('GET|POST /test', function() {
+$f3->route('GET|POST /personal-information', function() {
+    //from home
     $template = new Template();
-    echo $template->render('views/home.html');
+    echo $template->render('views/personal-information.html');
 }
 );
 
 
 //define a create profile info route
 $f3->route('GET|POST /create/profile-info', function() {
+    //from personal-information
+    $_SESSION['first'] = $_POST['first'];
+    $_SESSION['last'] = $_POST['last'];
+    $_SESSION['age'] = $_POST['age'];
+    $_SESSION['gender'] = $_POST['gender'];
+    $_SESSION['phone'] = $_POST['phone'];
     $template = new Template();
     echo $template->render('views/profile-info.html');
 }
@@ -48,6 +54,11 @@ $f3->route('GET|POST /create/profile-info', function() {
 
 //define an interests route
 $f3->route('GET|POST /create/interests', function() {
+    //from profile
+    $_SESSION['email'] = $_POST['email'];
+    $_SESSION['seeking'] = $_POST['seeking'];
+    $_SESSION['state'] = $_POST['state'];
+    $_SESSION['bio'] = $_POST['bio'];
     $template = new Template();
     echo $template->render('views/interests.html');
 }
@@ -55,6 +66,8 @@ $f3->route('GET|POST /create/interests', function() {
 
 //define a summary route
 $f3->route('GET|POST /summary', function() {
+    $_SESSION['indoor'] = $_POST['indoor'];
+    $_SESSION['outdoor'] = $_POST['outdoor'];
     $template = new Template();
     echo $template->render('views/summary.html');
 }
