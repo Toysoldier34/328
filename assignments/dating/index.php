8b5786056398c92a008823548ff3e067b503d7c4
Tony <tony.g.thompson@gmail.com>
2018-02-02 21:54:40 -0800

    Validation implemented

--- assignments/dating/index.php
+++ assignments/dating/index.php
@@ -44,6 +44,7 @@ $f3->route('GET|POST /personal-information', function() {
 $f3->route('GET|POST /profile-info', function() {
     //from personal-information
 
+    $template = new Template();
     if(isset($_POST['submit'])) {
         $first = $_SESSION['first'] = $_POST['first'];
         $last = $_SESSION['last'] = $_POST['last'];
@@ -51,13 +52,39 @@ $f3->route('GET|POST /profile-info', function() {
         $_SESSION['gender'] = $_POST['gender'];
         $phone = $_SESSION['phone'] = $_POST['phone'];
 
+
         include('model/validate.php');
+
+        $isValid = $fVal = $lVal = $aVal = $pVal = true;
+
+        if (!validName($first)) {
+            $isValid = $fVal = false;
+        }
+
+        if (!validName($last)) {
+            $isValid = $lVal = false;
+        }
+
+        if (!validAge($age)) {
+            $isValid = $aVal = false;
+        }
+
+        if (!validPhone($phone)) {
+            $isValid = $pVal = false;
+        }
+
+        if ($isValid) {
+            echo $template->render('views/profile-info.html');
+        } else {
+            if ($fVal) echo "<p>Enter a valid first name</p>";
+            if ($lVal) echo "<p>Enter a valid last name</p>";
+            if ($aVal) echo "<p>Enter a valid age over 18</p>";
+            if ($pVal) echo "<p>Enter a valid phone number</p>";
+            echo $template->render('views/personal-information.html');
+        }
+    } else {
+        echo $template->render('views/personal-information.html');
     }
-
-
-
-    $template = new Template();
-    echo $template->render('views/profile-info.html');
 }
 );
 
@@ -76,33 +103,48 @@ $f3->route('GET|POST /interests', function() {
 //define a summary route
 $f3->route('GET|POST /summary', function($f3) {
 
+    $template = new Template();
     if(isset($_POST['submit'])) {
     $indoor = $_SESSION['indoor'] = $_POST['indoor'];
     $outdoor = $_SESSION['outdoor'] = $_POST['outdoor'];
 
         include('model/validate.php');
+        $isValid = $iVal = $oVal = true;
 
+        if (!validOutdoor($outdoor)) {
+            $isValid = $iVal = false;
+        }
 
+        if (!validIndoor($indoor)) {
+            $isValid = $oVal = false;
+        }
+
+        if ($isValid) {
+            //set variables in hive
+            $f3->set('first', $_SESSION['first']);
+            $f3->set('last', $_SESSION['last']);
+            $f3->set('age', $_SESSION['age']);
+            $f3->set('gender', $_SESSION['gender']);
+            $f3->set('phone', $_SESSION['phone']);
+
+            $f3->set('email', $_SESSION['email']);
+            $f3->set('seeking', $_SESSION['seeking']);
+            $f3->set('state', $_SESSION['state']);
+            $f3->set('bio', $_SESSION['bio']);
+
+            $f3->set('indoor', $_SESSION['indoor']);
+            $f3->set('outdoor', $_SESSION['outdoor']);
+
+            echo $template->render('views/summary.html');
+        } else {
+            if ($oVal) echo "<p>Valid outdoor activities only</p>";
+            if ($iVal) echo "<p>Valid indoor activities only</p>";
+            echo $template->render('views/interests.html');
+        }
+    } else {
+        echo $template->render('views/interests.html');
     }
 
-
-    //set variables in hive
-    $f3->set('first', $_SESSION['first']);
-    $f3->set('last', $_SESSION['last']);
-    $f3->set('age', $_SESSION['age']);
-    $f3->set('gender', $_SESSION['gender']);
-    $f3->set('phone', $_SESSION['phone']);
-
-    $f3->set('email', $_SESSION['email']);
-    $f3->set('seeking', $_SESSION['seeking']);
-    $f3->set('state', $_SESSION['state']);
-    $f3->set('bio', $_SESSION['bio']);
-
-    $f3->set('indoor', $_SESSION['indoor']);
-    $f3->set('outdoor', $_SESSION['outdoor']);
-
-    $template = new Template();
-    echo $template->render('views/summary.html');
 }
 );
 
