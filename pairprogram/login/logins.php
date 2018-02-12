ae462b980f333cfac5434af02669746b49272653
Tony <tony.g.thompson@gmail.com>
2018-02-12 14:59:04 -0800

    variables and constructor setup

---
+++ pairprogram/login/logins.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * User: tonyg
+ * Date: 2/12/2018
+ * Time: 11:43 AM
+ */
+
+
+Class User {
+    private $_username;
+    private $_password;
+    private $_loggedIn;
+
+    function __construct()
+    {
+        $this->_username = "";
+        $this->_password = "";
+        $this->_loggedIn = false;
+    }//end constructor
+
+
+
+
+//end class
+}
