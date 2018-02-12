dcaab1e4de4c6ab2697e37b7641a2db3d91b8719
Tony <tony.g.thompson@gmail.com>
2018-02-12 15:20:05 -0800

    Part 1-5 login() setup and file contents corrected with user.php

--- pairprogram/login/logins.php
+++ pairprogram/login/logins.php
@@ -5,47 +5,4 @@
  * Time: 11:43 AM
  */
 
-
-Class User
-{
-    private $_username;
-    private $_password;
-    private $_loggedIn;
-
-    function __construct()
-    {
-        $this->_username = "";
-        $this->_password = "";
-        $this->_loggedIn = false;
-    }//end constructor
-
-
-    /**
-     * @return string
-     */
-    public function getUsername()
-    {
-        return $this->_username;
-    }
-
-    /**
-     * @param string $username
-     */
-    public function setUsername($username)
-    {
-        $this->_username = $username;
-    }
-
-    /**
-     * @param string $password
-     */
-    public function setPassword($password)
-    {
-        if (strlen($password) > 5) {
-            $this->_password = $password;
-        }
-    }
-
-
-//end class
-}
+$userList = array("user1"=>"pass1", "user2"=>"pass2", "user3"=>"pass3");
---
+++ pairprogram/login/user.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * User: tonyg
+ * Date: 2/12/2018
+ * Time: 11:43 AM
+ */
+
+
+Class User
+{
+    private $_username;
+    private $_password;
+    private $_loggedIn;
+
+    function __construct()
+    {
+        $this->_username = "";
+        $this->_password = "";
+        $this->_loggedIn = false;
+    }//end constructor
+
+
+    function login($username, $password)
+    {
+        $userList = array();
+        include 'logins.php';
+        if (in_array($username, $userList)) {
+            if ($userList['$username'] == $password) {
+            }
+            echo $username . " is logged in.";
+            $this->_loggedIn = true;
+            return;
+        }
+
+        echo "Login error.";
+    }
+
+
+    function logout()
+    {
+
+    }
+
+
+    /**** GETTERS SETTERS ****/
+
+    /**
+     * @return string
+     */
+    public
+    function getUsername()
+    {
+        return $this->_username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public
+    function setUsername($username)
+    {
+        $this->_username = $username;
+    }
+
+    /**
+     * @param string $password
+     */
+    public
+    function setPassword($password)
+    {
+        if (strlen($password) > 5) {
+            $this->_password = $password;
+        }
+    }
+
+
+//end class
+}
