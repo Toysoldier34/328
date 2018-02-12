cbe35e2eb9363587f52fdccf640cb15e1a1e5e96
Tony <tony.g.thompson@gmail.com>
2018-02-12 15:00:40 -0800

    Part 1-3 username getter setter

--- pairprogram/login/logins.php
+++ pairprogram/login/logins.php
@@ -19,6 +19,21 @@ Class User {
     }//end constructor
 
 
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->_username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->_username = $username;
+    }
 
 
 //end class
