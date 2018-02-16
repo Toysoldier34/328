3c7a7aa44d6e2a0411020cacb0a0b4410950a91e
Tony <tony.g.thompson@gmail.com>
2018-02-16 15:39:16 -0800

    premiumMember class

--- assignments/dating/classes/member.php
+++ assignments/dating/classes/member.php
@@ -32,7 +32,7 @@ Class Member
 
 
 
-    
+
 
 
 
---
+++ assignments/dating/classes/premiummember.php
@@ -0,0 +1,47 @@
+<?php
+
+Class PremiumMember extends Member{
+
+    protected $indoor;
+    protected $outdoor;
+
+
+
+
+
+
+    /********  SETTERS GETTERS  ********/
+
+    /**
+     * @return mixed
+     */
+    public function getIndoor()
+    {
+        return $this->indoor;
+    }
+
+    /**
+     * @param mixed $indoor
+     */
+    public function setIndoor($indoor)
+    {
+        $this->indoor = $indoor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOutdoor()
+    {
+        return $this->outdoor;
+    }
+
+    /**
+     * @param mixed $outdoor
+     */
+    public function setOutdoor($outdoor)
+    {
+        $this->outdoor = $outdoor;
+    }
+
+}//end class premiumMember
